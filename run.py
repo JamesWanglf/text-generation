0f2b3c3a6d5e0a2983c47ff82ba7c31cc37783bb
JamesWanglf <jameswanglf@outlook.com>
2022-07-18 10:30:19 -0400

    commit project

---
+++ run.py
@@ -0,0 +1,113 @@
+import base64
+import cv2
+import io
+import numpy as np
+import torch
+from datetime import datetime
+from flask import Flask, request, Response, jsonify, make_response
+from PIL import Image
+from image_captioning.models.blip import blip_decoder
+from torchvision import transforms
+from torchvision.transforms.functional import InterpolationMode
+
+app = Flask(__name__)
+model = None
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+IMAGE_PROCESS_OK = 100
+IMAGE_PROCESS_ERR = 101
+INVALID_REQUEST_ERR = 231
+INVALID_IMAGE_ERR = 232
+UNKNOWN_ERR = 500
+
+ERR_MESSAGES = {
+    IMAGE_PROCESS_OK: 'The image is processed successfully.',
+    IMAGE_PROCESS_ERR: 'The image process has been failed.',
+    INVALID_REQUEST_ERR: 'Invalid request.',
+    INVALID_IMAGE_ERR: 'Invalid image has input. Could not read the image data.',
+    UNKNOWN_ERR: 'Unknown error has occurred.'
+}
+
+
+def transform_raw_image(raw_image, image_size=384):
+    encoded_data = raw_image.split(',')[1]
+    decoded_string = io.BytesIO(base64.b64decode(encoded_data))
+    img = Image.open(decoded_string).convert('RGB')
+    img.save('gfg_dummy_pic.png')
+    
+    transform = transforms.Compose([
+        transforms.Resize((image_size,image_size),interpolation=InterpolationMode.BICUBIC),
+        transforms.ToTensor(),
+        transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
+    ])
+    image = transform(img).unsqueeze(0).to(device)
+    return image
+
+
+def load_model():
+    global model
+
+    image_size = 384
+    model_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/models/model_base_capfilt_large.pth'
+
+    model = blip_decoder(pretrained=model_url, image_size=image_size, vit='base')
+    model.eval()
+    model = model.to(device)
+
+
+def process_image(raw_image):
+    global model
+
+    # Transform the raw_image
+    image = transform_raw_image(raw_image)
+
+    with torch.no_grad():
+        # beam search
+        caption = model.generate(image, sample=False, num_beams=3, max_length=20, min_length=5)
+        print('caption: ' + caption[0])
+
+    return IMAGE_PROCESS_OK, caption[0]
+
+
+@app.route('/', methods=['GET'])
+def welcome():
+    """
+    Welcome page
+    """
+    return Response("<h1 style='color:red'>Image Captioning server is running!</h1>", status=200)
+
+
+@app.route('/image-captioning', methods=['GET', 'POST'])
+def face_recognition():
+    if request.method == 'GET':
+        return Response('Image Captioning  server is running.', status=200)
+
+    # POST
+    # Read image data
+    img_data = request.json
+    if 'image' not in img_data:
+        response = {
+            'error': ERR_MESSAGES[INVALID_REQUEST_ERR]
+        }
+        return make_response(jsonify(response), 400)
+
+    # Load model
+    if model is None:
+        load_model()
+
+    # Process image
+    start_time = datetime.now()
+    res_code, caption = process_image(img_data['image'])
+    print(f'Image process takes {datetime.now() - start_time}')
+
+    if res_code != IMAGE_PROCESS_OK:
+        response = {
+            'error': ERR_MESSAGES[res_code]
+        }
+        return make_response(jsonify(response), 500)
+
+    # Return candidates
+    response = {
+        'caption': caption
+    }
+    return make_response(jsonify(response), 200)
---
+++ test.py
@@ -0,0 +1,35 @@
+import base64
+import json
+import os
+import requests
+from datetime import datetime
+
+hostname = 'http://localhost:31000'
+dir_path = os.path.join(os.path.dirname(__file__), 'image_captioning', 'images')
+
+
+def image_captioning_test():
+    file_path = os.path.join(dir_path, f'test_image_1.png')
+    with open(file_path, 'rb') as img_file:
+        base64_img = base64.b64encode(img_file.read()).decode('utf-8')
+
+        img_data = {
+            'image': f'data:image/jpeg;base64,{base64_img}',
+        }
+
+    url = f'{hostname}/image-captioning'
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    payload = json.dumps(img_data)
+
+    response = requests.post(url, headers=headers, data=payload)
+
+    print(response.text)
+
+
+if __name__ == '__main__':
+    # Test image captioning with "test_image_1.png"
+    start_time = datetime.now()
+    image_captioning_test()
+    print(datetime.now() - start_time)
